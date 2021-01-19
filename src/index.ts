bcbf38bf4d2483c097e965ed15d060a79b9aa856
bertrand Blanquet <bertrand.blanquet6@gmail.com>
2021-01-19 19:53:34 +0100

    clean

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,6 @@ const app: e.Application = e();
 const httpServer = http.createServer(app);
 const ioContext = new IoContext(httpServer);
 httpServer.listen(5000,()=>{console.log("listen 5000")});
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
