91731c22717dbabcc30381cdb5b2a26425614aec
bertrand Blanquet <bertrand.blanquet6@gmail.com>
2021-02-16 23:22:52 +0100

    Clean #4

--- src/handlers/Handler.ts
+++ src/handlers/Handler.ts
@@ -3,8 +3,5 @@ import * as socketio from "socket.io";
 
 export abstract class Handler{
     constructor(protected roomManager:RoomManager,protected ioServer:socketio.Server){}
-    protected Print(message:string):void{
-        console.log(message);
-    }
     public abstract On(socket:socketio.Socket):void;
 }
