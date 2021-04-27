9aaa2d0fafdda074838fa8f9e798c4f4cf8d079f
bertrand Blanquet <bertrand.blanquet6@gmail.com>
2021-04-28 00:02:37 +0200

    ???

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import e from 'express';
 import { IoContext } from './IoContext';
 const port = process.env.PORT || 5000;
 new IoContext(http.createServer(e()).listen(port,function(){
-	console.log(`star listening on *:${port}`);
+	console.log(`STAR listening on *:${port}`);
 }));
